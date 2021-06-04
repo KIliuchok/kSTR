0fb0062fe0905c844e341c754de217ecb1119f78
KIliuchok <49845913+KIliuchok@users.noreply.github.com>
2021-06-04 20:39:42 +0800

    Proper Init with config template (No python code)

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const { exec } = require('child_process');
-const { response } = require('express');
+const { response, json } = require('express');
 const { stdout, stderr } = require('process');
-const db = require('./db_queries_users');
+const db_users = require('./db_queries_users');
+const db_videos = require('./db_queries_videos');
 const https = require('https');
-const http = require('http');
+const unirest = require('unirest');
+const fetch = require('node-fetch');
+
+var config = require('./config.js');
 
 const LogRocket = require('logrocket');
 
@@ -19,14 +23,15 @@ LogRocket.init('pcp3y1/website');
 // Certificate
 
 options = {
-key: fs.readFileSync("/etc/letsencrypt/live/kiliuchok.club/privkey.pem", 'utf8'),
-cert: fs.readFileSync("/etc/letsencrypt/live/kiliuchok.club/fullchain.pem", 'utf8')
+key: fs.readFileSync(config.secure.privkey_location, 'utf8'),
+cert: fs.readFileSync(config.secure.cert_location, 'utf8')
 };
 
 
 
 
 const app = express();
+const http = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
@@ -36,9 +41,8 @@ app.use(express.urlencoded());
 // File System get Requests I assume
 
     // Change to DB path TODO
-const assets = 'public/videoassets';
-
-videoName = 'attack.mp4';
+const assets = config.library;
+const images = config.images;
 
 
 //
@@ -47,42 +51,18 @@ videoName = 'attack.mp4';
 
 router.get('/', (req, res) => {
 
-    fs.access(assets+'/images/'+videoName+'.jpg', fs.F_OK, (err) => {
-
-        if (err) {
-            exec(`/Users/kostia/Desktop/Website/ffmpeg/ffmpeg -i ${assets}/${videoName.replace(/\s/g, '\ ')}.mp4 -ss 00:00:04.00 -r 1 -an -vframes 1 -f mjpeg ${assets}/images/${videoName.replace(/\s/g, '\ ')}.jpg`, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-
-                console.log("Thumbnail Redone");
-
-                res.render('index', {
-                    image: `/videoassets/images/${videoName}.jpg`,
-                    video_path: `/videoassets/attack.mp4`
-                });
-            });
-        }
-
-        if(err === null) {
-            res.render('index', {
-                image: `/videoassets/images/${videoName}.jpg`,
-                video_path: `/videoassets/attack.mp4`
-            });
-        }
-    });
+    res.render('index');
 });
 
 router.get('/videos/:videName', (req, res) => {
 
     console.log(req.params.videName);
     video_name = req.params.videName;
-    fs.access(assets+`/images/${video_name}.jpg`, fs.F_OK, (err) => {
+    fs.access(`${images}/${video_name}.jpg`, fs.F_OK, (err) => {
 
         if (err) {
           console.log(err)
-            exec(`/Users/kostia/Desktop/Website/ffmpeg/ffmpeg -i '${assets}/${videoName}' -ss 00:00:04.00 -r 1 -an -vframes 1 -f mjpeg '${assets}/images/${video_name}.jpg'`, (error, stdout, stderr) => {
+            exec(`ffmpeg/ffmpeg -i '${assets}/TV Shows/Miscellenious/${video_name}' -ss 00:00:10.00 -r 1 -an -vframes 1 -f mjpeg '${images}/${video_name}.jpg'`, (error, stdout, stderr) => {
                 if (error) {
                     console.log(error);
                     return;
@@ -91,26 +71,78 @@ router.get('/videos/:videName', (req, res) => {
                 console.log("Thumbnail Redone");
 
                 res.render('index', {
-                    image: `/videoassets/images/${req.params.videName}.jpg`,
-                    video_path: `/video/${video_name}`
+                    image: `/${images}/${video_name}.jpg`,
+                    video_path: `/video/${video_name}`,
+                    list_of_all_shows: `/getShowNames`
                 });
             });
         } else {
             console.log("Here")
             res.render('index', {
-                image: `/videoassets/images/${req.params.videName}.jpg`,
-                video_path: `/video/${video_name}`
+                image: `/${images}/${req.params.videName}.jpg`,
+                video_path: `/video/${video_name}`,
+                list_of_all_shows: `/getShowNames`,
             });
         }
     });
 });
 
+router.get('/video/:showName/:epName', (req, res) => {
+    console.log("This got accessed");
+
+    const showName = req.params.showName;
+    const epName = req.params.epName;
+
+    const path = `${assets}/TV Shows/${showName}/${epName}`
+    console.log("Path to file " + path)
+
+    fs.stat(path, (err, stat) => {
+
+        // Handle file not found
+        if (err !== null && err.code === 'ENOENT') {
+            res.sendStatus(404);
+        }
+
+        const fileSize = stat.size
+        const range = req.headers.range
+
+        if (range) {
+
+            const parts = range.replace(/bytes=/, "").split("-");
+
+            const start = parseInt(parts[0], 10);
+            const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+
+            const chunksize = (end-start)+1;
+            const file = fs.createReadStream(path, {start, end});
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            }
+
+            res.writeHead(206, head);
+            file.pipe(res);
+        } else {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            }
+
+            res.writeHead(200, head);
+            fs.createReadStream(path).pipe(res);
+        }
+    });
+})
 
 router.get('/video/:videName', (req, res) => {
 
     console.log("This got accessed");
     const path = `${assets}/TV Shows/Miscellenious/${req.params.videName}`;
     console.log(path);
+
+    console.log("In main file:")
 
     fs.stat(path, (err, stat) => {
 
@@ -158,16 +190,247 @@ router.get('/video/:videName', (req, res) => {
 
 // Users part
 
-router.get('/users', db.getUsers)
-router.get('/users/:id', db.getUserById)
-router.post('/users', db.createUser)
-router.put('/users/:id', db.updateUser)
-router.delete('/users/:id', db.deleteUser)
+router.get('/users', db_users.getUsers)
+router.get('/users/:id', db_users.getUserById)
+router.post('/users', db_users.createUser)
+router.put('/users/:id', db_users.updateUser)
+router.delete('/users/:id', db_users.deleteUser)
+
+router.get('/getShowNames', db_videos.getShowNames)
+
+router.get('/dir', (req, res) => {
+    exec(`python3.9 ${__dirname}/python_module/get_all_shows.py`, (err, stdout, stderr) =>{
+        if (err) {
+            throw err
+        }
+        
+        var string_of_shows = stdout;
+        var list_of_tmdb_results = [];
+        var list_of_shows = string_of_shows.split(',')
+        list_of_shows.pop(list_of_shows.length)
+        var list_of_urls = []
+
+        list_of_shows.forEach(element => {
+            list_of_urls.push(`/shows/${element}`)
+        });
+
+
+        var list_of_images = []
+        var dict = {}
+        
+        list_of_shows.forEach(element =>{
+            var temp = element.slice(0,15)
+            fetch(`https://api.themoviedb.org/3/search/tv?api_key=881ec62b5040a4be19ab4f6e242a4624&language=en-US&page=1&query=${temp}&include_adult=true`)
+            .then(res => res.json())
+            .then(json => {
+                if (typeof json.results[0] === 'undefined') {
+                    list_of_images.push(`/${images}/${element}/preview.jpg`)
+                    dict[element] = {poster : `/${images}/${element}/preview.jpg`,
+                    show_name : element,
+                    overview : "No overview available",
+                    aired : "No data available"}
+                } else {
+                    list_of_images.push(`https://image.tmdb.org/t/p/w500${json.results[0].poster_path}`)
+                    var truncated = json.results[0].overview
+
+                    if (truncated.length > 275) {
+                        truncated = truncated.substr(0,275) + '...'
+                    }
+
+                    dict[element] = {poster : `https://image.tmdb.org/t/p/w500${json.results[0].poster_path}`,
+                                    show_name : json.results[0].name,
+                                    overview : truncated,
+                                    aired: json.results[0].first_air_date}
+                }
+
+                if (list_of_images.length === list_of_shows.length) {
+                    var temp = []
+                    var _shows = [] 
+                    var _img = []
+                    var _show_name = []
+                    var _overview = []
+                    var _aired = []                  
+                    
+                    for(var key in dict) {
+                        temp.push(key)
+                    }
+
+                    temp.sort()
+                    console.log(temp)
+
+                    for (var key of temp) {
+                        _shows.push(key)
+                        _img.push(dict[key].poster)
+                        _overview.push(dict[key].overview)
+                        _show_name.push(dict[key].show_name)
+                        _aired.push(dict[key].aired)
+                    }
+
+                    console.log(_aired)
+
+
+                    res.render('dir', {
+                        list_of_shows: _shows,
+                        list_of_images: _img,
+                        show_name: _show_name,
+                        overview: _overview,
+                        aired: _aired
+                    })
+                }
+            }
+
+            )
+        })
+    })
+})
+
+
+
+
+router.get('/shows/:showName', (req, res) => {
+    console.log(req.params.showName)
+    showName = req.params.showName;
+
+    console.log(TVInfo(`${showName}`.slice(0,15)))
+
+    exec(`python3.9 python_module/get_all_eps.py '${req.params.showName}'`, (err, stdout, stderr) =>{
+        if (err) {
+            throw err
+        }
+
+        var list_of_eps = stdout.split(',')
+        list_of_eps.pop(list_of_eps.length)
+        const ep_images = []
+        
+        list_of_eps.forEach(element => {
+            fs.access(`${images}/${showName}/${element}.jpg`, fs.F_OK, (err) => {
+
+                if (err) {
+                  console.log(err)
+                    exec(`${__dirname}/ffmpeg/ffmpeg -i '${assets}/TV Shows/${showName}/${element}' -ss 00:00:10.00 -r 1 -an -vframes 1 -f mjpeg 'images/${showName}/${element}.jpg'`, (error, stdout, stderr) => {
+                        if (error) {
+                            console.log(error);
+                            return;
+                        }
+        
+                        console.log("Thumbnail Redone");
+        
+                        ep_images.push(`/${images}/${showName}/${element}.jpg`)
+
+                    });
+                } else {
+                    console.log("There is an image for " + `${element}`)
+                }
+            });
+        })
+
+        list_of_eps.forEach(element => {
+            ep_images.push(`/${images}/${showName}/${element}.jpg`)
+        })
+        
+        console.log(list_of_eps)
+        console.log(ep_images)
+
+        res.render('show', {
+            list_of_eps: list_of_eps,
+            ep_images: ep_images,
+            showName: req.params.showName,
+        })
+        
+    })
+})
+
+router.get('/shows/:showName/:episode', (req, res) => {
+    console.log(req.params.videName);
+
+    showName = req.params.showName;
+    epName = req.params.episode;
+
+    fs.access(`${images}/${showName}/${epName}.jpg`, fs.F_OK, (err) => {
+
+        if (err) {
+          console.log(err)
+            exec(`ffmpeg/ffmpeg -i '${assets}/TV Shows/${showName}/${epName}' -ss 00:00:10.00 -r 1 -an -vframes 1 -f mjpeg '/${images}/${showName}/${epName}.jpg'`, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+
+                console.log("Thumbnail Redone");
+
+                res.render('episode_play', {
+                    image: `/${images}/${showName}/${epName}.jpg`,
+                    video_path: `/video/${showName}/${epName}`
+                });
+            });
+        } else {
+            console.log("Here")
+            res.render('episode_play', {
+                image: `/${images}/${showName}/${epName}.jpg`,
+                video_path: `/video/${showName}/${epName}`,
+                ep_name: `${req.params.episode}`
+            });
+        }
+    });
+})
+
+
+// The Movie DB API to find info about shows, movies
+
+
+function TVInfo(showName) {
+
+    var request = unirest("GET", "https://api.themoviedb.org/3/search/tv");
+
+    request.query({
+        "api_key": "881ec62b5040a4be19ab4f6e242a4624",
+        "page": "1",
+        "query": `${showName}`,
+        "include_adult": "true"
+    }
+    )
+    request.end(function (res) {
+
+        if (res.error) throw new Error(res.error);
+
+        return res.body.results
+        
+    });
+
+}
+
+	
+
+
+
+// REGISTERING AND LOGGING IN FUNCTIONALITY
+
+router.get('/login',(req,res)=>{
+    res.render('login');
+})
+router.get('/register',(req,res)=>{
+    res.render('register')
+})
+
+router.post('/register', (req,res) => {
+    
+})
+
+router.post('/login', (req,res,next) => {
+
+})
+
+router.get('/logout', (req, res) => {
+    
+})
+
+
+
 
 //
 
 setInterval(() =>{
-    exec (`python3.9 /Users/kostia/Desktop/Website/public/parse_files.py`, (err, stdout, stderr) =>{
+    exec (`python3.9 ${__dirname}/python_module/parse_files.py`, (err, stdout, stderr) =>{
         if (err) {
             throw err
         }
@@ -179,12 +442,29 @@ setInterval(() =>{
 
 app.use(router);
 
-const PORT = process.env.PORT || 443;
+app.enable('trust proxy');
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+});
+
+const PORT = process.env.PORT || config.ports.https;
 
 
 var server = https.createServer(options, app).listen(PORT, function(){
   console.log("Express server listening on port " + PORT);
 });
+
+//HTTP redirect
+
+http.get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+})
+
+// have it listen on 8080
+http.listen(config.ports.http);
 
 //app.listen(PORT, () => {
 //    console.log(`Server listening on port ${PORT}`);
